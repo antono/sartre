ee2df79e94bc049ec3ba633bb7d0d9c98e2aa35c
Antono Vasiljev <antono.vasiljev@gmail.com>
2015-11-21 10:16:27 +0300

    list helper

--- examples/example.scm
+++ examples/example.scm
@@ -1,7 +1,7 @@
 (import (sartre sxml)
         (sartre router))
 
-(define framework-list
+(define framework-table
   '((Framework Language Note)
     (Sinatra   Ruby     Simple)
     (Rails     Ruby     Fat)
@@ -9,13 +9,21 @@
     (Valum     Vala     Fast)
     (Sartre    Scheme   Experimental)))
 
+(define list-items
+  '(ul ol li))
+
 (define (hello-handler request body)
   (values '((content-type . (text/html)))
           (lambda (port)
-            (list->html-table framework-list port))))
+            (list->html-table framework-table port))))
 
 (define (world-handler request body)
   (values '((content-type . (text/plain))) "World"))
+
+(define (list-handler request body)
+  (values '((content-type . (text/html)))
+    (lambda (port)
+      (list->html-list list-items port))))
 
 (define (main-handler request body)
   (values '((content-type . (text/html)))
@@ -30,11 +38,13 @@
                 (ul
                  (li (a (@ (href "/hello")) "Hello"))
                  (li (a (@ (href "/world")) "World"))
+                 (li (a (@ (href "/list")) "List"))
                  (li (a (@ (href "http://community.schemewiki.org/?sxml-example")) "SXML")))))
              port))))
 
 (route 'GET "/hello" hello-handler)
 (route 'GET "/world" world-handler)
+(route 'GET "/list"  list-handler)
 (route 'GET "/" main-handler)
 
 (sartre 8080)
--- sartre/sxml.scm
+++ sartre/sxml.scm
@@ -2,14 +2,14 @@
   #:version (0 0 1)
   #:use-module (sxml simple)
   #:export (list->html-table
-             make-sxml-table
-             make-sxml-table-row
-             make-sxml-table-column)
+            list->html-list)
   #:re-export (sxml->xml))
 
 ;;
 ;; HTML Formatting Helpers
-;; 
+;;
+
+;; Table helpers
 (define (list->html-table table port)
   (sxml->xml (make-sxml-table table) port))
 
@@ -21,3 +21,13 @@
 
 (define (make-sxml-table-column value)
   `(td ,value))
+
+;; List helpers
+(define (list->html-list list port)
+  (sxml->xml (make-sxml-list list) port))
+
+(define (make-sxml-list list)
+  `(ul ,(map make-sxml-list-item list)))
+
+(define (make-sxml-list-item value)
+  `(li ,value))
